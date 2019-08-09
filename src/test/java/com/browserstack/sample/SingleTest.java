8efa6cf4ef6876a17c5dbf67bbafac23a3f53fd0
Renan Vicente <renanvice@gmail.com>
2019-08-09 16:13:06 +1200

    upgrade selenium

--- src/test/java/com/browserstack/sample/SingleTest.java
+++ src/test/java/com/browserstack/sample/SingleTest.java
@@ -3,6 +3,8 @@ package com.browserstack.sample;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
 import org.testng.annotations.Test;
 
 import org.testng.Assert;
@@ -13,6 +15,9 @@ public class SingleTest extends BrowserStackTestNGTest {
     public void test() throws Exception {
         driver.get("https://www.google.com/ncr");
         WebElement element = driver.findElement(By.name("q"));
+        SessionId session = ((RemoteWebDriver)driver).getSessionId();
+        System.out.println("Session id: " + session.toString());
+
         element.sendKeys("it works");
         element.submit();
         Thread.sleep(5000);
