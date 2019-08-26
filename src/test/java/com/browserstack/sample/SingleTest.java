bd12193e5e5da6b72683cce19bc47794eb7c822a
Renan Vicente <renanvice@gmail.com>
2019-08-26 14:46:22 +1200

    adding json obj

--- src/test/java/com/browserstack/sample/SingleTest.java
+++ src/test/java/com/browserstack/sample/SingleTest.java
@@ -2,7 +2,9 @@ package com.browserstack.sample;
 
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -11,7 +13,9 @@ import org.testng.annotations.Test;
 
 import org.testng.Assert;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class SingleTest extends BrowserStackTestNGTest {
 
@@ -23,10 +27,19 @@ public class SingleTest extends BrowserStackTestNGTest {
         try (FileReader reader = new FileReader("parameters.json")) {
 
             Object obj = jsonParser.parse(reader);
-            //JSONArray json = (JSONArray) obj;
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray data = ( JSONArray ) jsonObject.get("testParameters");
 
-            System.out.println(obj);
+            for ( Object o: data) {
+                System.out.println(o);
+            }
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
         driver.get("https://www.google.com/ncr");
         WebElement element = driver.findElement(By.name("q"));
@@ -40,5 +53,23 @@ public class SingleTest extends BrowserStackTestNGTest {
         Assert.assertEquals("it works - Google Search", driver.getTitle());
 
     }
+
+//    private static void parseTestParametersObject(JSONObject employee)
+//    {
+//        //Get employee object within list
+//        JSONObject employeeObject = (JSONObject) employee.get("testParameters");
+//
+//        //Get employee first name
+//        String firstName = (String) employeeObject.get("testName");
+//        System.out.println(firstName);
+//
+//        //Get employee last name
+//        String lastName = (String) employeeObject.get("testFunctionName");
+//        System.out.println(lastName);
+//
+//        //Get employee website name
+//        JSONObject website = (JSONObject) employeeObject.get("testParameters");
+//        System.out.println(website);
+//    }
 }
 
